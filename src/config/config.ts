7f1d60c4b6157af912c29b7053c717736330e86e
Lior Chen <lior@iokun.com>
2018-05-22 15:31:35 +0300

    feat(config): allow dotenv override from process

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,17 +1,17 @@
 import dotenv from 'dotenv';
 // Parse dotenv variables. Note: this will also go to process.env
-const { parsed: env } = dotenv.config();
+dotenv.config();
 
 const toBoolean = (str: string): boolean => str !== undefined ? JSON.parse(str) : false;
 const toNumber = (str: string): number => str !== undefined ? parseInt(str, 10) : 0;
 
 export default {
-  GOOGLE_CLIENT_ID: <string> env.GOOGLE_CLIENT_ID,
-  MONGO_URI: <string> env.MONGO_URI,
-  MULTI_USER: <boolean> toBoolean(env.MULTI_USER),
-  SERVER_IP: <string> env.SERVER_IP,
-  SERVER_API_PORT: <number> toNumber(env.SERVER_API_PORT),
-  SSL_KEY_FILE: <string> env.SSL_KEY_FILE,
-  SSL_CERT_FILE: <string> env.SSL_CERT_FILE,
-  WEBSOCKET_PORT: <number> toNumber(env.WEBSOCKET_PORT),
+  GOOGLE_CLIENT_ID: <string> process.env.GOOGLE_CLIENT_ID,
+  MONGO_URI: <string> process.env.MONGO_URI,
+  MULTI_USER: <boolean> toBoolean(process.env.MULTI_USER),
+  SERVER_IP: <string> process.env.SERVER_IP,
+  SERVER_API_PORT: <number> toNumber(process.env.SERVER_API_PORT),
+  SSL_KEY_FILE: <string> process.env.SSL_KEY_FILE,
+  SSL_CERT_FILE: <string> process.env.SSL_CERT_FILE,
+  WEBSOCKET_PORT: <number> toNumber(process.env.WEBSOCKET_PORT),
 };
